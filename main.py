2e0b1dd5ce9cd52566ab465aff209f6d622f5d16
Utkarsh <utkarshpath@gmail.com>
2026-09-01 19:34:48 +0530

    Fix CORS allow_origins to Vercel domain

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://bulletin-balaji-pharma.vercel.app", "http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
